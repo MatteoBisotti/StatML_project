68646a4307b62c415999d5b39ee3598966b7f81a
MatteoBisotti <matteo.bisotti@studenti.unimi.it>
2026-04-02 21:54:17 +0200

    implementation methods for KMeans class

--- code/script/kmeans.py
+++ code/script/kmeans.py
@@ -16,14 +16,58 @@ class KMeans:
 
         self.centroids = None
 
-        self.rng = np.random.default_rng(random_state)
-
     def initialize_centroids(self, X):
         """
-        Randomly choose K distincs points from X as initial centroids.
+        Randomly choose K distincs points from X as initial centroids
         """
         n_samples = len(X)
 
-        indices = self.rng.choice(n_samples, size=self.n_clusters, replace=False)
+        indices = np.random.choice(n_samples, size=self.n_clusters, replace=False)
         centroids = X[indices].copy()
         return centroids
+    
+    def euclidean_distances(self, X, centroids):
+        """
+        Compute squared Euclidean distances from each point to each centroid
+
+        Returns: distances[i, j] = ||X[i] - centroids[j]||^2
+        """
+        distances = []
+
+        for x in X:
+            row = []
+            for c in centroids:
+                row.append(np.sum((x-c) ** 2))
+            distances.append(row)
+        
+        return np.array(distances)
+    
+    def assign_clusters(self, X, centroids):
+        """
+        Assign each point to the nearest centroid
+        """
+        distances = self.euclidean_distances(X, centroids)
+        labels = np.argmin(distances, axis=1)
+        return labels
+
+    def update_centroids(self, X, labels, old_centroids):
+        """
+        Update centroids as the mean of assigned points
+        """
+        n_features = X.shape[1]
+        new_centroids = np.empty((self.n_clusters, n_features), dtype=float)
+
+        for k in range(self.n_clusters):
+            cluster_points = X[labels == k]
+
+            if len(cluster_points)==0:
+                new_centroids[k] = old_centroids[k]
+            else:
+                new_centroids[k] = np.mean(cluster_points, axis=0)
+        
+        return new_centroids
+    
+    def compute_inertia(self, X, labels, centroids):
+        """
+        Compute the inertia 
+        """
